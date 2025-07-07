2de2a6c5fbe12cc5d85521024945677747f27fca
Vilhelms <103843693+villhelmms@users.noreply.github.com>
2025-07-07 11:37:49 +0300

    rofi

--- symlinks.sh
+++ symlinks.sh
@@ -22,6 +22,10 @@ setup_dotfiles() {
 
 setup_dotfiles
 
+# ------------------------ #
+# ------ HYPR FILES ------ #
+# ------------------------ #
+
 HYPR_HOME="$HOME/.config/hypr"
 HYPR_DOT="$HOME/.dotfiles/hypr"
 
@@ -45,6 +49,10 @@ if [ -d "$HYPR_HOME" ]; then
         fi
     done
 fi
+
+# -------------------------- #
+# ------ WAYBAR FILES ------ #
+# -------------------------- #
 
 WAYBAR_HOME="$HOME/.config/waybar"
 WAYBAR_DOT="$HOME/.dotfiles/waybar"
@@ -70,13 +78,86 @@ if [ -d "$WAYBAR_HOME" ]; then
     done
 fi
 
+# ------------------------ #
+# ------ ROFI FILES ------ #
+# ------------------------ #
+
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+chmod +x /rofi/setup.sh
+./rofi/setup.sh
+
+ROFI_HOME="$HOME/.config/rofi"
+ROFI_DOT="$HOME/.dotfiles/rofi"
+
+if [ ! -d "$ROFI_HOME" ]; then
+    echo "Directory $ROFI_HOME does not exist. Creating it now."
+    mkdir -p "$ROFI_HOME"
+fi
+
+create_symlink() {
+    local src_file="$1"
+    local dest_file="$2"
+
+    if [ -f "$dest_file" ]; then
+        echo "Backing up existing file: $dest_file"
+        mv "$dest_file" "$dest_file.old"
+    fi
+
+    echo "Creating symlink for: $(basename "$src_file")"
+    ln -s "$src_file" "$dest_file"
+}
+
+COLORS_DIR="$ROFI_DOT/colors"
+if [ -d "$COLORS_DIR" ]; then
+    mkdir -p "$ROFI_HOME/colors"
+    mkdir -p "$ROFI_HOME/launchers/type-1/shared"
+    mkdir -p "$ROFI_HOME/powermenu/type-1/shared"
+
+    for color_file in "$COLORS_DIR"/*; do
+        if [ -f "$color_file" ]; then
+            if [[ "$(basename "$color_file")" == "ondark-custom.rasi" ]]; then
+                create_symlink "$color_file" "$ROFI_HOME/colors/$(basename "$color_file")"
+            elif [[ "$(basename "$color_file")" == "colors.rasi" ]]; then
+                create_symlink "$color_file" "$ROFI_HOME/launchers/type-1/shared/$(basename "$color_file")"
+                create_symlink "$color_file" "$ROFI_HOME/powermenu/type-1/shared/$(basename "$color_file")"
+            fi
+        fi
+    done
+fi
+
+LAUNCHERS_DIR="$ROFI_DOT/launchers"
+if [ -d "$LAUNCHERS_DIR" ]; then
+    mkdir -p "$ROFI_HOME/launchers/type-1"
+
+    for launcher_file in "$LAUNCHERS_DIR"/*; do
+        if [ -f "$launcher_file" ]; then
+            create_symlink "$launcher_file" "$ROFI_HOME/launchers/type-1/$(basename "$launcher_file")"
+        fi
+    done
+fi
+
+POWERMENU_DIR="$ROFI_DOT/powermenu"
+if [ -d "$POWERMENU_DIR" ]; then
+    mkdir -p "$ROFI_HOME/powermenu/type-1"
+
+    for powermenu_file in "$POWERMENU_DIR"/*; do
+        if [ -f "$powermenu_file" ]; then
+            create_symlink "$powermenu_file" "$ROFI_HOME/powermenu/type-1/$(basename "$powermenu_file")"
+        fi
+    done
+fi
+
+# ------------------------------- #
+# ------ KITTY / ZSH FILES ------ #
+# ------------------------------- #
+
+KITTY_HOME="$HOME/.config/kitty"
+KITTY_DOT="$HOME/.dotfiles/kitty"
+
 if [ ! -d "$KITTY_HOME" ]; then
     echo "Directory $KITTY_HOME does not exist. Creating it now."
     mkdir -p "$KITTY_HOME"
 fi
-
-KITTY_HOME="$HOME/.config/kitty"
-KITTY_DOT="$HOME/.dotfiles/kitty"
 
 if [ -d "$KITTY_HOME" ]; then
     for DOT_FILE in "$KITTY_DOT"/*; do
@@ -94,18 +175,7 @@ if [ -d "$KITTY_HOME" ]; then
     done
 fi
 
-# if [ "$SHELL" != "$(which zsh)" ]; then
-#     echo "Changing default shell to zsh..."
-#     chsh -s "$(which zsh)"
-
-#     echo "Default shell changed to zsh. Please log out and log back in for the changes to take effect."
-# else
-#     echo "The default shell is already set to zsh."
-# fi
-
 kitty --hold bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
